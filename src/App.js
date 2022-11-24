82ace24e621fa45039663250ed0d561328682f14
sibelkoc <sibel.koc.pro@outlook.fr>
2022-11-24 21:19:38 +0100

    📦 create component with contructor + ➿  random hand with math.random for a random hand + add the front in place

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import React, { Component } from 'react';
 import './App.css';
+import AllHands from "./AllHands";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theHand: ["rock", "paper", "scissors"],
+      Kylie: "",
+      BadGrammord: "",
+      theWinner: ""
+    }
+  }
+
+  BadGrammordHand = () => {
+    this.setState({ BadGrammord: this.state.theHand[Math.floor(Math.random() * 3)] },() => {
+      this.theWinner();
+    })
+  }
+
+ 
+
+  render() {
+    return (
+      <div className="App">
+
+        <div className="theHands">
+        {this.state.theHand.map((element, index) => {
+          return <AllHands  function={this.click} key={index} img={element}/>
+        })}
+        </div>
+
+
+      
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
